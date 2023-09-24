742e7fe27b20cab2f7eb0dab23619741ad4b8e72
andersjac <97388757+andersjac@users.noreply.github.com>
2023-09-24 12:26:34 +0100

    now updated for Henrik's fan controller

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
-basic.showIcon(IconNames.Yes)
-servos.P1.setAngle(180)
-basic.pause(5000)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
 basic.forever(function () {
-    basic.showNumber(pins.analogReadPin(AnalogPin.P0))
-    servos.P1.setAngle(Math.map(pins.analogReadPin(AnalogPin.P0), 460, 1023, 0, 180))
+    servos.P1.setAngle(Math.map(pins.analogReadPin(AnalogPin.P0), 428, 1023, 0, 180))
 })
